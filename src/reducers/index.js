30ee3e0d3226ed6fa52a59249d1652931c785218
Mikel <mikel362d@gmail.com>
2022-11-07 18:35:03 -0800

    adds PostsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
     //adds dummy key so there's no warning when reducers are empty
-    replaceMe: () => 10
+    posts: postsReducer
 });
