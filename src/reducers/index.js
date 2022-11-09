87b2ce47b8ddaa68abbf5431270ddba639c69bb1
Mikel <mikel362d@gmail.com>
2022-11-08 18:41:52 -0800

    display name of blogger

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
-import postsReducer from "./postsReducer";
+import { postsReducer } from "./postsReducer";
+import { usersReducer } from "./usersReducer";
 
 export default combineReducers({
-    //adds dummy key so there's no warning when reducers are empty
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer
 });
