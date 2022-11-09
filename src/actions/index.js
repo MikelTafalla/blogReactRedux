568b585bba7e412716b36bb14f86118bab5a9f16
Mikel <mikel362d@gmail.com>
2022-11-09 08:31:19 -0800

    fix overfecthing without using memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,31 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-//underscore to indicate it is a private funcion
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+};
+
+
+///Example on how to do memoization. Drawback, we can only fetch users one time in our whole app
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// //underscore to indicate it is a private funcion
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
